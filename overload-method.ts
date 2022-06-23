53902f3bb5c1a968edec86df771109fde28ee561
Самойлов Александр Сергеевич <samoylov_a@goldapple.ru>
2022-06-23 23:45:10 +0500

    ex. 5; Перегрузка методов

---
+++ overload-method.ts
@@ -0,0 +1,13 @@
+class User {
+    skills: string[];
+
+    addSkill(skill: string): void;
+    addSkill(skills: string[]): void;
+    addSkill(skillOrSkills: string | string[]): void {
+        if (typeof skillOrSkills == 'string') {
+          this.skills.push(skillOrSkills)
+        } else {
+          this.skills.concat(skillOrSkills)
+        }
+    }
+}
